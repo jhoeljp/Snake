664690819e451d20a0f8da815d45b81a8404df70
Jhoel Perez <jhoel.jjpr@gmail.com>
2021-03-22 12:32:19 +0300

    eat mechanic
    apple hit and body movement fixed

--- main.py
+++ main.py
@@ -21,12 +21,12 @@ class Point():
     def __init__(self,x,y):
         self.x = x
         self.y = y
-    def tuple_form(self):
-        return (self.x,self.y)
+    def list_form(self):
+        return [self.x,self.y]
     def val_x(self):
-        return self.x
+        return int(self.x)
     def val_y(self):
-        return self.y
+        return int(self.y)
 
 #build Snake board
 DISPLAY.fill(WHITE)
@@ -48,9 +48,11 @@ def random_draw(color):
     #generate random apple on map
     x,y = random.randint(0, N-1), random.randint(0, N-1)
     #update global apple location value
-    apple_pos = Point(x,y)
+    pos = Point(x,y)
     #draw rectangle
     color_square(x,y,color)
+    #return random pos coordinates
+    return pos
 
 def color_square(x,y,color):
     #convert to board coordinates
@@ -60,10 +62,13 @@ def color_square(x,y,color):
     pygame.draw.rect(DISPLAY,color,[margin1,margin2,WIDTH,HEIGHT])
 
 def movement(dir):
-    new_pos_x = snake_pos[0].val_x
-    new_pos_y = snake_pos[0].val_y
+    global apple_pos
+
+    new_pos_x = snake_pos[0].val_x()
+    new_pos_y = snake_pos[0].val_y()
     #delete prev snake
     color_square(new_pos_x,new_pos_y,BLACK)
+    print("Blacking: ",snake_pos[0].list_form())
     #augment coordinates based on snake's direction
     if dir == 'N':
         new_pos_y = new_pos_y - 1
@@ -74,36 +79,46 @@ def movement(dir):
     if dir == 'W':
         new_pos_x = new_pos_x - 1
 
-    #eating apple
-    print(snake_pos, " -- ", str(apple_pos.tuple_form())
+    #eating apple phase
+
     #no hit
-    if snake_pos[0].tuple_form() != apple_pos.tuple_form():
+    if snake_pos[0].list_form() != apple_pos.list_form():
+        blk = snake_pos[len(snake_pos)-1]
+        color_square(blk.val_x(),blk.val_y(),BLACK)
         snake_pos.pop()
     #hit
     else:
         print("hit")
-        random_draw(APPLE_color)
+        #new apple location
+        apple_pos = random_draw(APPLE_color)
 
     #tail moves
-    snake_pos.append(Point(new_pos_x,new_pos_y))
+    snake_pos.insert(0,(Point(new_pos_x,new_pos_y)))
     #to new position
-    if new_x <= N-1 and new_y<= N-1:
-        if new_x >= 0 and new_y >= 0:
-            color_square(new_x,new_y,SNAKE_color)
+    if new_pos_x <= N-1 and new_pos_y<= N-1:
+        if new_pos_x >= 0 and new_pos_y >= 0:
+            for bod in snake_pos:
+                # color_square(new_pos_x,new_pos_y,SNAKE_color)
+                color_square(bod.val_x(),bod.val_y(),SNAKE_color)
 
     else:
         pygame.quit()
         sys.exit()
+    tmp = ''
+    for i in range(0,len(snake_pos)):
+        tmp += str(snake_pos[i].list_form())
+    print(tmp)
 
 #start game variables
 loop = True
 #Apple object location
-apple_pos = Point(0,0)
-random_draw(APPLE_color)
+# apple_pos = Point(0,0)
+apple_pos = random_draw(APPLE_color)
+# print(apple_pos)
 
 #Snake body location
-snake_pos = [Point(0,0)]
-random_draw(SNAKE_color)
+snake_pos = []
+snake_pos.append(random_draw(SNAKE_color))
 
 direction = ''
 
